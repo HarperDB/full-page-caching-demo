3e5df535c06862ee5b50ac3f7bfaf30bdc1c767d
keyology <keonimurray45@gmail.com>
2024-10-01 14:10:55 -0400

    first commit

--- resources.js
+++ resources.js
@@ -1,3 +1,6 @@
+
+const {PageCache, MyCache} = tables;
+
 /** Here we can define any JavaScript-based resources and extensions to tables
 
 export class MyCustomResource extends tables.TableName {
@@ -20,3 +23,71 @@ export class Greeting extends Resource {
 		return { greeting: 'Hello, world!' };
 	}
 }
+
+const parseCacheControl = (cacheContolHeader) => {
+
+}
+
+export class WebPageCacheResource extends Resource {
+	async get() {
+		try{
+
+			const htmlContent = `
+						<div>
+							<h1>Hello, World!</h1>
+							<p>This is a sample HTML response in JSON format.</p>
+						</div>
+    		`;
+
+			//return {html: htmlContent }
+
+			return{
+				id: "/testPage",
+				cachedData: htmlContent
+			}
+			
+		}catch(e){
+			console.log("CACHING ERROR", e);
+		}
+		
+	}
+}
+
+
+export class testPageCache extends Resource {
+	invalidate() {
+		super.invalidate();
+	  }
+
+	async get(){
+		
+		try{
+			console.log("FETCHING PAGE");
+			const response = (await fetch(`https://www.google.com/`));
+			console.log("RESPONSE", response);
+			const rawHtmltoStr = await response.text();
+            console.log("RESPONSE", rawHtmltoStr);
+			
+            return { id: "/testPage", cachedData: rawHtmltoStr };
+		}catch(e){
+			console.log("ERROR", e);
+		}
+		
+	}
+}
+MyCache.sourcedFrom(testPageCache, { replicationSource: true });
+
+
+
+
+// you can access the cache from the browser using the following URL: http://localhost:9926/MyCache/testPage
+
+
+// TODO: need to clean up this repo and turn it into a template 
+
+//TODO:  need to set expiration time for cache
+
+//TODO:  need to configure when cache needs to be updated in table
+
+// TODO: need to create a README.md file with instructions on how to implement partial page caching
+
