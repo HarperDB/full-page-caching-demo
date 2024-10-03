2b90d6688f27b5daabf5bdf721b2472c16303094
keyology <keonimurray45@gmail.com>
2024-10-03 11:34:08 -0400

    added code to only return the html as a string and nothing else needs clean up

--- resources.js
+++ resources.js
@@ -1,12 +1,84 @@
-
+// import page cache table
 const {PageCache} = tables;
 
+
+/*
+
+TODO: Example of how to cache a page
+
+export class testPageCache extends Resource {
+	invalidate() {
+		super.invalidate();
+	  }
+
+	async get(){
+		
+		try{
+			
+			const response = (await fetch(`https://www.google.com/`));
+			const rawHtmltoStr = await response.text();
+
+            return { id: "/testPage", cachedData: rawHtmltoStr };
+
+		}catch(e){
+			console.log("ERROR", e);
+		}
+		
+	}
+}
+
+PageCache.sourcedFrom(testPageCache);
+
+//http://localhost:9926/PageCache/testPage
+
+
+
+
+*/
+
+
+
+export class testPageCache extends Resource {
+	invalidate() {
+		super.invalidate();
+	  }
+
+	async get(){
+		
+		try{
+			
+			const response = (await fetch(`https://www.google.com/`));
+			const rawHtmltoStr = await response.text();
+
+            return { id: "/testPage", cachedData: rawHtmltoStr };
+
+		}catch(e){
+			console.log("ERROR", e);
+		}
+		
+	}
+}
+
+PageCache.sourcedFrom(testPageCache);
+
+export class Pc extends PageCache {
+	get() {
+		let context = this.getContext();
+		// console.log("-----------", this)
+		// console.log("------------")
+		console.log("---CONTEXT", context, "--------CONTEXT 2------")
+		return this.cachedData
+
+		// http://localhost:9926/Pc/testPage
+	  
+	  }
+	}
 
 
 const parseCacheControl = (cacheContolHeader) => {
 
 	// let cacheInfo = response.headers.get('Cache-Control');
-    //   // TODO: This line parses cache control header to find out how loing the data should be cached 
+    //   // TODO: This line parses cache control header to find out how long  the data should be cached 
     //   if (cacheInfo) {
     //     let maxAge = cacheInfo?.match(/max-age=(\d+)/)?.[1];
 
@@ -29,11 +101,16 @@ const parseCacheControl = (cacheContolHeader) => {
 }
 
 // The code below is the a class that is used to update the cache
-export class PageCacheResource extends Resource {
+export class PageCacheResource extends PageCache {
 
 	invalidate() {
 		super.invalidate();
 	  }
+
+	  // TODO: 
+	  //const cacheId = this.getId()
+	  //pageURL = 'http://somehost.com/' + cacheId
+	  // response.byte as an optimization
 	
 	async get() {
 		try{
@@ -50,7 +127,7 @@ export class PageCacheResource extends Resource {
 	}
 }
 
-PageCache.sourcedFrom(PageCacheResource, { replicationSource: true });
+PageCache.sourcedFrom(PageCacheResource);
 
 // you can access the cache from the browser using the following URL: http://localhost:9926/MyCache/testPage
 
@@ -64,4 +141,49 @@ PageCache.sourcedFrom(PageCacheResource, { replicationSource: true });
 //TODO:  need to configure when cache needs to be updated in table
 
 // TODO: need to create a README.md file with instructions on how to implement partial page caching
+
+
+
+// TODO: 
+
+//export class PageCache extends tables.PageCache {
+// 	get() {
+// 		return this.html;
+//    }
+
+
+// export class PageCache extends tables.PageCache {
+// 	get() {
+// 	  return { // can also return a full response header
+// 		 status: 200,
+// 		 headers: { 'Content-Type': 'text/html' },
+// 		 body: this.html
+// 		}
+// 	  }
+// 	}
+
+
+
+// http://host.com/PageCache/testPage.html
+
+
+
+// Put some thought into 
+
+// Naming component based on the the functionality of the component
+
+
+// fumctionality intent behind each component 
+
+// extenstion
+
+
+// TODO: 
+
+// What distinguishes each component from the other 
+
+
+// intent of use vs functionality of a component
+
+
 
